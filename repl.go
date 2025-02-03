d9270ce7d9d35c4595a24636addfca68959044c3
John Eatwell <>
2025-02-03 08:25:25 +0200

    Adding Unit Tests

---
+++ repl.go
@@ -0,0 +1,11 @@
+package main
+
+import "strings"
+
+func cleanInput(text string) []string {
+	text = strings.TrimSpace(text)
+	if text == "" {
+		return []string{}
+	}
+	return strings.Fields(strings.ToLower(text))
+}
