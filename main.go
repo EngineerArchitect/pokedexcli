245ad951e433de243054115027e194b658571bcf
John Eatwell <>
2025-02-03 08:38:40 +0200

    Help and Exit

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		if !scanner.Scan() {
-			break
-		}
-		words := cleanInput(scanner.Text())
-		if len(words) > 0 {
-			fmt.Printf("Your command was: %s\n", words[0])
-		}
-	}
+	startRepl()
 }
